fbd6ef9923a44c3a3a032e085207068a11c28835
Leora Waldman <leorawaldman@gmail.com>
2024-11-02 23:08:26 +0200

    changed message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import PrintingArea from "./components/PrintingArea";
-import { useState } from "react";
 
 function App() {
   const [recipeArr, updateRecipeArr] = useState([]);
@@ -23,7 +23,7 @@ function App() {
   return (
     <>
       <div className="heading">
-        <h1>Recipe App2</h1>
+        <h1>Recipe App3</h1>
       </div>
       <div className="container">
         <Form onSubmit={addRecipe}></Form>
